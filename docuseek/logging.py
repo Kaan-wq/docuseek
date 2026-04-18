cab9f1ebdcb9f6fc4ac985a90d2d4dead98f6708
Kaan-wq <kaanucar9@gmail.com>
2026-04-18 18:13:36 +0200

    feat : logging config

---
+++ docuseek/logging.py
@@ -0,0 +1,14 @@
+# docuseek/logging.py
+import structlog
+
+
+def configure_logging(log_level: str = "info") -> None:
+    structlog.configure(
+        processors=[
+            structlog.contextvars.merge_contextvars,
+            structlog.processors.add_log_level,
+            structlog.processors.TimeStamper(fmt="iso"),
+            structlog.dev.ConsoleRenderer(),
+        ],
+        wrapper_class=structlog.make_filtering_bound_logger(log_level),
+    )
